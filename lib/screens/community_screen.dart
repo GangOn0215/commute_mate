0148e9af98c888a47b474b210402b1d4af1af75d
GangOn0215 <96044518+GangOn0215@users.noreply.github.com>
2025-10-21 23:53:11 +0900

    [ c ] community 에 ListView.builder 써보기

--- lib/screens/community_screen.dart
+++ lib/screens/community_screen.dart
@@ -1,15 +1,40 @@
 import 'package:flutter/material.dart';
 
-class CommunityPage extends StatefulWidget {
-  const CommunityPage({super.key});
+class CommunityScreen extends StatefulWidget {
+  const CommunityScreen({super.key});
 
   @override
-  State<CommunityPage> createState() => _CommunityPageState();
+  State<CommunityScreen> createState() => _CommunityScreenState();
 }
 
-class _CommunityPageState extends State<CommunityPage> {
+class _CommunityScreenState extends State<CommunityScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Community'),
+        elevation: 0,
+        backgroundColor: Colors.white,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.notifications_none_outlined),
+          ),
+        ],
+      ),
+      body: Center(
+        child: ListView.builder(
+          itemCount: 1000,
+          itemBuilder: (BuildContext context, int index) {
+            return ListTile(
+              leading: Icon(Icons.person),
+              title: Text('Post $index'),
+              subtitle: Text('This is the detail of post $index.'),
+              trailing: Icon(Icons.arrow_forward_ios),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
