bfc9051ba49b2750202f9a8d70c33632f5770b89
GangOn0215 <ekdldktnekf@gmail.com>
2025-11-02 19:17:36 +0900

    [ DTO ] Post Update Request

---
+++ spring-app/hello/src/main/java/com/example/hello/dto/PostUpdateRequest.java
@@ -0,0 +1,12 @@
+package com.example.hello.dto;
+
+
+import com.example.hello.common.enums.PostsCategory;
+import lombok.Data;
+
+@Data
+public class PostUpdateRequest  {
+    private String title;
+    private String content;
+    private PostsCategory category;
+}
