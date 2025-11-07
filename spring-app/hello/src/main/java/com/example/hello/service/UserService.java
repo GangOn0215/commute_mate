b9cb1508813dbd8dc93c04b35a70aa32a5235f22
GangOn0215 <ekdldktnekf@gmail.com>
2025-11-07 20:40:26 +0900

    [ c ] 스프링 User Service 에 upload image 처리

--- spring-app/hello/src/main/java/com/example/hello/service/UserService.java
+++ spring-app/hello/src/main/java/com/example/hello/service/UserService.java
@@ -1,11 +1,14 @@
 package com.example.hello.service;
 
+import com.example.hello.dto.UploadResponse;
 import com.example.hello.entity.User;
 import com.example.hello.repository.UserRepository;
+import jdk.jfr.TransitionFrom;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
@@ -15,6 +18,8 @@ import java.util.List;
 public class UserService {
     private final PasswordEncoder passwordEncoder;
     private final UserRepository repo;
+    private final UserRepository userRepository;
+    private final FileStorageService fileStorageService;
 
     public User signup(User user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
@@ -36,6 +41,39 @@ public class UserService {
 
         // 패스워드 변환
         return found;
+    }
+
+    public UploadResponse uploadProfileImage(Long userId, MultipartFile file) {
+        validateImageFile(file);
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다."));
+
+        if(user.getProfileImageUrl() != null) {
+            fileStorageService.deleteFile(user.getProfileImageUrl());
+        }
+
+        String imageUrl = fileStorageService.storeFile(file, "profiles");
+
+        user.setProfileImageUrl(imageUrl);
+        userRepository.save(user);
+
+        return new UploadResponse(
+          "이미지 업로드 성공",
+          imageUrl,
+          file.getSize()
+        );
+    }
+
+    private void validateImageFile(MultipartFile file) {
+        if(file.isEmpty()) {
+            throw new IllegalArgumentException("파일이 비었습니다.");
+        }
+
+        String contentType = file.getContentType();
+        if(contentType == null || !contentType.startsWith("image/")) {
+            throw new IllegalArgumentException("이미지 파일만 업로드 가능합니다.");
+        }
     }
 
     public List<User> getAll() {
