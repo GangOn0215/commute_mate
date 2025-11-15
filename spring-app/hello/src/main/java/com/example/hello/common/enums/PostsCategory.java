d4e7a65d4c529bfb1e541c5fd4dd094d056ffb80
GangOn0215 <ekdldktnekf@gmail.com>
2025-11-15 22:30:53 +0900

    [ u ] Post Enum, 클라이언트로 JSON 넘길 때 한글로 보낼 수 있게 처리

--- spring-app/hello/src/main/java/com/example/hello/common/enums/PostsCategory.java
+++ spring-app/hello/src/main/java/com/example/hello/common/enums/PostsCategory.java
@@ -1,9 +1,23 @@
 package com.example.hello.common.enums;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum PostsCategory {
-    GENERAL,
-    QUESTION,
-    COMPANY,
-    COMMUTE,
-    CAT
+    GENERAL("일반"),
+    QUESTION("질문"),
+    COMPANY("회사"),
+    COMMUTE("출퇴근"),
+    CAT("고양이");
+
+    private final String description;
+
+    // 이 메서드가 JSON 직렬화 시 사용됨
+    @JsonValue
+    public String getDescription() {
+        return description;
+    }
 }
