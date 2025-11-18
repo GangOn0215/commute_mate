14e842c66855682fc8d0c00e1ccd1eb2496cf5fb
GangOn0215 <96044518+GangOn0215@users.noreply.github.com>
2025-11-19 07:47:37 +0900

    [ c ] Flutter 댓글 작업

--- flutter-app/lib/screens/community/community_view.dart
+++ flutter-app/lib/screens/community/community_view.dart
@@ -23,7 +23,57 @@ class CommunityView extends StatelessWidget {
       ),
       body: Padding(
         padding: EdgeInsets.all(16.0),
-        child: PostDetailCard(post: post),
+        child: Column(
+          children: [
+            PostDetailCard(post: post),
+            SizedBox(height: 16.0),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: const Color.fromARGB(255, 24, 21, 21).withAlpha(16),
+                    spreadRadius: 2,
+                    blurRadius: 5,
+                    offset: Offset(0, 3),
+                  ),
+                ],
+              ),
+
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        SizedBox(width: 8),
+                        Text(
+                          '댓글',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(width: 4),
+                        Text('3', style: TextStyle(fontSize: 16)),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        // 여기에 댓글 위젯들을 추가하세요
+                        SizedBox(height: 8),
+                        Text('댓글 기능은 아직 구현되지 않았습니다.'),
+                        SizedBox(height: 8),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
